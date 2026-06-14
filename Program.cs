3473784bd22e832d3d222fe51454072736f23db9
Kaca353 <katarinacugurovic5@gmail.com>
2026-06-14 09:42:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+using BibliotekaMVC.Data;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BibliotekaContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("BibliotekaConnection")));
+
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<BibliotekaContext>();
+
+    // sve knjige ponovo dostupne
+    foreach (var k in context.Knjige)
+    {
+        k.Dostupna = true;
+    }
+
+    // briši sva zaduženja
+    context.Zaduzenja.RemoveRange(context.Zaduzenja);
+
+    context.SaveChanges();
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
